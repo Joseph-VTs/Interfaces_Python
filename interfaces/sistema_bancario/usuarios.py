0345f854929e6d060add3e57f1f65521dbd490ff
Joseph-VTs <compcopycode@gmail.com>
2025-09-24 13:33:00 -0300

    Create usuarios.py

---
+++ interfaces/sistema_bancario/usuarios.py
@@ -0,0 +1,18 @@
+# Simulação de "banco de dados" de usuários
+
+usuarios = {
+    "12345678900": {
+        "senha": "senha123",
+        "nome": "João",
+        "saldo": 1500.0,
+        "extrato": []
+    },
+    "98765432100": {
+        "senha": "abc123",
+        "nome": "Maria",
+        "saldo": 2300.0,
+        "extrato": []
+    }
+}
+
+usuario_logado = None
